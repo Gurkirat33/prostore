fe9e53279ee33c64b1411758d5c6cdc15a108cdd
Gurkirat Singh <gurkiratsingh13a@gmail.com>
2025-07-17 17:06:42 -0600

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ["lib/generated/**"], //ignore the generated code on  build
+    ignores: ["src/generated/**"],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
